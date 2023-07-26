63215f40673f2566e3d5a45fc21840c5570107bf
udit goyal <139154194+uditgoyal12@users.noreply.github.com>
2023-07-26 13:06:08 +0530

    Homepage of Netflix

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-const celsiusElem = document.querySelector("#celsius");
-const degree = document.querySelector("#degree");
-const convertBtn = document.querySelector("#convert-btn");
-const tempType = document.querySelector("#temp-type");
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-window.addEventListener("load", () => {
-  degree.value = "";
-  celsiusElem.innerHTML = "";
-});
-
-convertBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  convertToCelsius();
-});
-
-function convertToCelsius() {
-  let inputValue = degree.value;
-
-  if (tempType.value === "fahrenheit") {
-    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
-    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
-  } else if (tempType.value === "kelvin") {
-    const KelvinToCelsius = inputValue - 273.15;
-    celsiusElem.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
-  }
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
 }
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
